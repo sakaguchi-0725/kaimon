a0d99c666dec87356e4e6a84592901b389332f69
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-06-24 18:06:04 +0900

    feat: welcome画面実装

---
+++ frontend/src/shared/auth/lib/index.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react"
+import * as SecureStore from "expo-secure-store"
+
+export const useAuth = () => {
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await SecureStore.getItemAsync("accessToken")
+      setIsAuth(!!token)
+    }
+    checkAuth()
+  }, [])
+
+  return { isAuth }
+}

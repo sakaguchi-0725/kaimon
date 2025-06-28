a805ba2fcdefb2fe00476105be264b1d9bc4da04
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-06-28 18:41:22 +0900

    feat: アカウント情報入力画面実装

--- frontend/src/screens/auth/ui/stack-navigator.tsx
+++ frontend/src/screens/auth/ui/stack-navigator.tsx
@@ -2,11 +2,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { WelcomeScreen } from './welcome'
 import { SignUpScreen } from './signup'
 import { LoginScreen } from './login'
+import { AccountInfoScreen } from './account-info'
 
 export type AuthStackParamList = {
   Welcome: undefined
   SignUp: undefined
   Login: undefined
+  AccountInfo: undefined
 }
 
 const Stack = createNativeStackNavigator<AuthStackParamList>()
@@ -17,6 +19,7 @@ export const AuthStackNavigator = () => {
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="AccountInfo" component={AccountInfoScreen} />
     </Stack.Navigator>
   )
 }
